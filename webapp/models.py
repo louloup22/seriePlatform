bf59ae756c4645440d94753880ae928778af1a98
Louise Picard <louise.picard@student.ecp.fr>
2018-10-11 22:08:09 +0200

    Django and user config

---
+++ webapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+#import config
+import json
+import requests
+from pandas import DataFrame as df
+from datetime import date
+import re
+
+
+
+
+
+
---
+++ webapp/urls.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 11 20:01:57 2018
+
+@author: LouiseP
+"""
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+        path('',views.home),
+        path('login/',auth_views.login,{'template_name': 'webapp/login.html'}),
+        path('logout/',auth_views.logout,{'template_name': 'webapp/logout.html'}),
+        path('signup/', views.signup, name='signup'),
+        ]
+
